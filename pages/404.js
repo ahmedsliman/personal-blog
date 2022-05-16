a79f07b1c27899e5758e1d0a0b0a19a19983643e
Ahmed.Abouelenin <ahmed.abouelenin@MX100671.fritz.box>
2022-05-16 12:07:49 +0200

    Pages with Markdown

---
+++ pages/404.js
@@ -0,0 +1,5 @@
+import utilStyles from '../styles/utils.module.css'
+
+export default function Custom404() {
+  return <h2 className={utilStyles.lightText} style={{textAlign: 'center'}}>404 - Page Not Found</h2>;
+}
